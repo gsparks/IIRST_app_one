6a02f53996cc7e8bc6132750d170685e1feba5e3
greg <gsparks@westmont.edu>
2013-01-30 22:04:23 -0800

    Added page by page load time monitor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  def authorize
+    if current_user.is_admin? 
+      Rack::MiniProfiler.authorize_request
+    end
+  end
+
 end
