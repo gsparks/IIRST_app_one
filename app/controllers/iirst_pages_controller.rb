4b801082d34154cdea3248ba7711b44173544d97
greg <gsparks@westmont.edu>
2013-02-12 22:09:18 -0800

    garbage collection

--- app/controllers/iirst_pages_controller.rb
+++
@@ -1,2 +0,0 @@
-class IirstPagesController < ApplicationController
-end
--- app/models/page_request.rb
+++
@@ -1,3 +0,0 @@
-class PageRequest < ActiveRecord::Base
-  attr_accessible :nifty, :path, :page_duration, :view_duration, :db_duration
-end
