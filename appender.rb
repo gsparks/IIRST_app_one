990fcb4e97d516d06ac2859cd625b3f9102f956f
johnny4shure <davidshure79@gmail.com>
2013-04-10 19:45:49 -0700

    The Appender module is done.

--- appender.rb
+++ appender.rb
@@ -1,36 +1,33 @@
 class Appender
 
-attr_reader :code, :destination_type, :path
+  attr_reader :code, :destination_type, :path
 
-    def initialize(path, code, mode, erase_all)
-      @path = path
-      @code = code
-      insert(mode, erase_all)
-    end
+  def initialize(path, code, mode, erase_all)
+    @path = path
+    @code = code
+    insert(mode, erase_all)
+  end
 
-    def insert(mode, start_over)
-      File.new(@path, "w") if start_over
-      if (mode == 'bottom')
-        if (@path)
-          File.open(@path, 'a+') { |file| file.puts @code }
-        end
-      elsif (mode == 'top')
-        if (@path)
-          newfile = File.new(@path + ".new","w")
-          newfile.puts @code
-          
-          oldfile = File.open(@path, "r+")
-          oldfile.each_line { |line| newfile.puts line}
-          
-          oldfile.close()
-          newfile.close()
-          
-          File.rename(@path + ".new", @path)
-        end
+  def insert(mode, start_over)
+    File.new(@path, "w") if start_over
+    if (mode == 'bottom')
+      if (@path)
+        File.open(@path, 'a+') { |file| file.puts @code }
+      end
+    elsif (mode == 'top')
+      if (@path)
+        newfile = File.new(@path + ".new","w")
+        newfile.puts @code
+        
+        oldfile = File.open(@path, "r+")
+        oldfile.each_line { |line| newfile.puts line}
+        
+        oldfile.close()
+        newfile.close()
+        
+        File.delete(@path)
+        File.rename(@path + ".new", @path)
       end
     end
   end
-  
-
-
-appender = Appender.new("./app/views/layouts/application.html.erb", "the cake is a lie", 'top', false)
+end
